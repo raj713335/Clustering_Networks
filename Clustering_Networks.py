3b978260ac4b61289f83d6aea45bfa6f89be72c3
ARNAB DAS <raj713335@gmail.com>
2019-02-07 01:56:58 +0530

    Update Clustering_Networks.py

--- Clustering_Networks.py
+++ Clustering_Networks.py
@@ -1,7 +1,12 @@
 import random
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class clustering_networks:
+
+
+    #particles = []
 
 
     def __init__(self):
@@ -18,15 +23,18 @@ class clustering_networks:
             x_cordinate=round(random.random()*100,2)          #creates the x cordinate of the particle in the range 0-100 units
             y_cordinate=round(random.random()*100,2)         #creates the y cordinate of the particles in the range 0-100 units
             #print(x_cordinate,y_cordinate)
-            no_of_neighbour=random.randint(0,10)        #randomly generates the number of neighbour a ith particle can have
+            no_of_neighbour=random.randint(0,no_of_particles-1)        #randomly generates the number of neighbour a ith particle can have and its always -1 than no of particles
             neighbor_temporary_list=[]
-            for i in range(no_of_neighbour):
-                temp=random.randint(0,no_of_particles)
-                neighbor_temporary_list.append(temp)
+            for j in range(no_of_neighbour):
+                temp=random.randint(0,no_of_particles-1)            #randomly generates the neighbour and its always no_of_particles-1 as randint is inclusive
+                if i!=j:                                            #ensures that a particle isnt a neighbour of itself
+                    neighbor_temporary_list.append(temp)
             particles.append([id,x_cordinate, y_cordinate,neighbor_temporary_list])  # append the position of particles in a list named particle and its neighbour
-        self.print_particles(self,particles)
+        self.print_particles(self,particles)                    #a function call to go to print_particles function defination
 
         #print(particles)
+
+        self.particles_visualization(self,particles)            #visualization of particles
 
     def print_particles(self,particles):
 
@@ -34,6 +42,11 @@ class clustering_networks:
         #Printing individual particles
         for each in particles:
             print("ID : {} X-Cordinate and Y-Cordinate : {} {} and neighbour : {}".format(each[0],each[1],each[2],each[3]))
+
+
+    def particles_visualization(self,particles):
+        pass
+
 
 
 
